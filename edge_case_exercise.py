75420062ceddb6c582808a8a5dc94f04e545d4dc
tamarbreuer11-commits <tamarbreuer11@gmail.com>
2026-02-25 17:51:39 +0200

    Implement edge case handling for move function
    Added edge case checks for moving '1' in both directions.

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -2,14 +2,18 @@ def move(my_list, direction):
 
     # Finds the index of the one in the list
     index_of_one = my_list.index(1)
-
     # Move the one to the left or to the right
     if direction == 'right':
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
-
+    # chek if one is in the edge
+       if index_of_one == len(my_list) -1:
+           return my_list
+       else  my_list[index_of_one] = 0
+             my_list[index_of_one + 1] = 1
+            return my_list
     elif direction == 'left':
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
-
-    return my_list
+    # chek if one is in the edge
+       if index_of_one == 0:
+           return my_list
+        else my_list[index_of_one] = 0
+             my_list[index_of_one - 1] = 1
+            return my_list
